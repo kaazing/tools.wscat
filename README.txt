6d0147e503b186b48999830ae2f6c6180273907f
Arthur Housinger <arthur.housinger@kaazing.com>
2014-11-17 07:51:55 +0000

    Migration to open source, initial commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+To run:
+mvn clean verify
+java -jar wscat-${project.version}.jar ws://echo.websocket.org/
---
+++ src/main/java/org/kaazing/tools/WSCat.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2007-2014, Kaazing Corporation. All rights reserved.
+ */
+
+package org.kaazing.tools;
+
+import org.kaazing.net.ws.WebSocket;
+import org.kaazing.net.ws.WebSocketFactory;
+import org.kaazing.net.ws.WebSocketMessageReader;
+import org.kaazing.net.ws.WebSocketMessageType;
+import org.kaazing.net.ws.WebSocketMessageWriter;
+import org.kaazing.net.http.HttpRedirectPolicy;
+
+import java.net.URI;
+import java.nio.ByteBuffer;
+import java.io.InputStream;
+import java.io.IOException;
+
+public class WSCat {
+
+    private static WebSocketFactory wsFactory;
+    private static WebSocket webSocket;
+
+    public static void main(String[] args) {
+	if (args.length < 1) {
+	    System.err.println("wscat <url>");
+	    return;
+	}
+
+	try {
+	    URI location = new URI(args[0]);
+	    
+	    wsFactory = WebSocketFactory.createWebSocketFactory();
+	    wsFactory.setDefaultRedirectPolicy(HttpRedirectPolicy.ALWAYS);
+	    
+	    webSocket = wsFactory.createWebSocket(location);
+	    webSocket.connect();
+	    
+	    Thread t = new Thread() {
+		    @Override
+		    public void run() {
+			try {
+			    WebSocketMessageWriter messageWriter = webSocket.getMessageWriter();
+			    
+			    InputStream in = System.in;
+			    byte b[] = new byte[8192];
+			    
+			    for (;;) {
+				int n = in.read(b);
+				if (n < 0) {
+				    break;
+				}
+				else if (n > 0) {
+				    ByteBuffer buf = ByteBuffer.wrap(b, 0, n);
+				    messageWriter.writeBinary(buf);
+				}
+			    }
+			} catch (IOException e) {
+			    e.printStackTrace();
+			}
+		    }
+		};
+	    t.start();
+	    
+	    final WebSocketMessageReader messageReader = webSocket.getMessageReader();
+	    WebSocketMessageType type = null;
+	    while ((type = messageReader.next()) != WebSocketMessageType.EOS) {
+		switch (type) {
+		case BINARY:
+		    ByteBuffer data = messageReader.getBinary();
+		    while (data.position() < data.limit()) {
+			System.out.write(data.get());
+		    }
+		    break;
+		case TEXT:
+		    CharSequence text = messageReader.getText();
+		    System.out.print(text.toString());
+		    break;
+		}
+	    }
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+    }
+}
